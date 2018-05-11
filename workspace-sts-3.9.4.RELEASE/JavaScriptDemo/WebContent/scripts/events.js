ac57f1937676cffa8e3470fbf48f7526b3f6b3a1
rtruong2 <rtruong2@gmu.edu>
2018-05-11 18:17:44 -0400

    idk it's friday

--- workspace-sts-3.9.4.RELEASE/JavaScriptDemo/WebContent/scripts/events.js
+++ workspace-sts-3.9.4.RELEASE/JavaScriptDemo/WebContent/scripts/events.js
@@ -10,4 +10,14 @@ window.onload = function() {
 		document.getElementById("mouseX").innerHTML = event.clientX;
 		document.getElementById("mouseY").innerHTML = event.clientX;
 	}
+	
+	var myEventHandler = function(event) {
+		console.log("target:" + event.target.id);
+	}
+	
+	var displayDivs = document.getElementsByClassName("displayDiv");
+	
+	for (var i = 0; i < displayDivs.length; i++) {
+		displayDivs[i].addEventListener("click", myEventHandler);
+	}
 }
---
+++ workspace-sts-3.9.4.RELEASE/JavaScriptDemo/ajaxEtc.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+var toggle = setInterval(function() {
+	console.log("called interval");
+	setColor();. 300);
+
+function setColor() {
+	var x = document.getElementById("toggleDiv")};
+	x.style.backgroundColor = x.style.backgroundColor == 'cyan' ? '#87fa4b' : cyan;
+);
+
+function stopColor(){
+	clearInteval(toggle);}
+}
